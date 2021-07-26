7c87605d6ce089d95cc06303faf3bedf33c6e2b3
Caio Braga <caiobs.99@gmail.com>
2021-07-26 18:06:59 -0300

    refactor db package to connect on 'init' function

--- db/db.go
+++ db/db.go
@@ -12,24 +12,16 @@ import (
 var DB *gorm.DB
 
 func init() {
+	var err error
 	name := os.Getenv("MYSQL_DATABASE")
 	host := os.Getenv("DB_HOST")
 	user := os.Getenv("DB_USER")
 	pass := os.Getenv("DB_PASS")
 
-	// Formats database connection string:
-	// username:password@protocol(address)/dbname?param=value
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", user, pass, host, name)
-	setupDB(dsn)
-}
-
-func setupDB(dsn string) {
-	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatalf("Cannot connect to database \n%s", err.Error())
 	}
-
-	print("ok")
 }
