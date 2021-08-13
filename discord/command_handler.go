a2075c67c6074d6196a3ee4bf77e37f6305b9898
Fernando <fernandoasfj+github@gmail.com>
2021-08-13 13:27:39 +0200

    Ignore messages generated by bino itself

--- discord/command_handler.go
+++ discord/command_handler.go
@@ -10,9 +10,16 @@ const UNKOWN_COMMAND_MESSAGE = "Desculpa, não entendi o seu comando"
 var mentorshipCommand = commands.Mentorship{}
 
 func CommandMessageHandler(session *discordgo.Session, message *discordgo.MessageCreate) {
+	if messageIsFromBotItself(session, message) {
+		return
+	}
 	response := processMessage(message.Content)
 	session.ChannelMessageSend(message.ChannelID, response)
 
+}
+
+func messageIsFromBotItself(session *discordgo.Session, message *discordgo.MessageCreate) bool {
+	return message.Author.ID == session.State.User.ID
 }
 
 func processMessage(messageContent string) string {
